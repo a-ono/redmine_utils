624ae46481ec7dae57f496af3be6790cc33b5cd3
Akihiro Ono <akihiro@ase.co.jp>
2013-07-24 16:42:58 +0900

    Version 0.1.1

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_utils do
   name 'Redmine Utils plugin'
   author 'Akihiro Ono'
   description 'Some extensions of Redmine'
-  version '0.1.0'
+  version '0.1.1'
   requires_redmine :version_or_higher => '2.3.0'
   url 'https://github.com/a-ono/redmine_utils'
 end
